a35e41bbce31cf50d0007d930955b6a79b0e9791
FireGhostPhino <Jackspaar@outlook.at>
2023-10-24 10:51:22 +0200

    Unit Tests Projekt angelegt

---
+++ MTCG-3.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace MTCG_3.Test
+{
+    public class MTCGUnitTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+            //Assert.Fail();
+
+        }
+    }
+}
